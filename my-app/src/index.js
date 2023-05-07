33518015bdb3a567f099204ae56f1f455c1e4440
TristanKensinger <tristan.kensinger@gmail.com>
2023-05-07 16:00:10 -0400

    implemented routes using react-router-dom for the frontend react app

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,11 +1,25 @@
-import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
-import App from "./App";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import NoPage from "./pages/NoPage";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="register" element={<Register />} />
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ my-app/src/pages/NoPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function NoPage() {
+  return <h1>this is the nopage</h1>;
+}
+
+export default NoPage;
